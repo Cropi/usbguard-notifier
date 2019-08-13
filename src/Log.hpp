cd1bb7ac933c32d51e06d9c179ec528e9c047553
Cropi <alakatos@redhat.com>
2019-08-13 08:53:28 +0200

    Removed unnecessary files and content

--- src/Log.hpp
+++ src/Log.hpp
@@ -4,37 +4,37 @@
 
 namespace usbguardNotifier
 {
-    class LoggerStream;
-    class Logger
-    {
-    public:
-        Logger();
-        LoggerStream createLogMessage(const std::string& file, const std::string& function, int line);
+class LoggerStream;
+class Logger
+{
+public:
+    Logger();
+    LoggerStream createLogMessage(const std::string& file, const std::string& function, int line);
+};
+
+class LoggerStream : public std::ostringstream
+{
+public:
+    struct Source {
+        const std::string& file;
+        const std::string& function;
+        const int line;
     };
 
-    class LoggerStream : public std::ostringstream
-    {
-    public:
-        struct Source {
-            const std::string& file;
-            const std::string& function;
-            const int line;
-        };
+    LoggerStream(Logger& logger, Source source);
+    LoggerStream(const LoggerStream& rhs);
+    ~LoggerStream();
 
-        LoggerStream(Logger& logger, Source source);
-        LoggerStream(const LoggerStream& rhs);
-        ~LoggerStream();
+private:
+    void writeLogMessage();
 
-    private:
-        void writeLogMessage();
+    Logger _logger;
+    Source _source;
+};
 
-        Logger _logger;
-        Source _source;
-    };
+extern Logger g_nLog;
 
-    extern Logger g_nLog;
-
- #define NOTIFIER_LOG() \
+#define NOTIFIER_LOG() \
     usbguardNotifier::g_nLog.createLogMessage(__BASE_FILE__, __func__, __LINE__)
 
 } /* namespace usbguard-notifier */
--- src/Notifier.hpp
+++ src/Notifier.hpp
@@ -1,10 +1,4 @@
-#include "IPCMessage.hpp"
-
-#include <qb/qbipcc.h>
-#include <qb/qbloop.h>
-
 #include <iostream>
-#include <map>
 
 #include <usbguard/IPCClient.hpp>
 
@@ -12,8 +6,8 @@ namespace usbguardNotifier
 {
     class SignalWatcher : public usbguard::IPCClient
     {
-
     public:
+
         void DevicePolicyChanged(
             uint32_t id,
             usbguard::Rule::Target target_old,
@@ -33,49 +27,4 @@ namespace usbguardNotifier
             const std::string& value_new);
     };
 
-    class Notifier;
-    using MessageObserver = IPC::MessageObserver<Notifier>;
-
-    class Notifier
-    {
-    public:
-        Notifier();
-        ~Notifier();
-        void start();
-        void stop();
-        std::map<uint32_t, MessageObserver> getObservers();
-
-        qb_ipcc_connection_t* qbGetCon();
-
-    private:
-
-        template<class T>
-        void createObserver(const uint32_t observer_type, MessageObserver::methodType method)
-        {
-            // TODO
-            _observers.emplace(observer_type, MessageObserver::create<T>(*this, method));
-        }
-
-        void handleDevicePolicyChanged(IPC::MessagePointer& request, IPC::MessagePointer& response);
-        void handleDeviceAvailabilityChanged(IPC::MessagePointer& request, IPC::MessagePointer& response);
-
-        qb_ipcc_connection_t* _qb_conn;
-        qb_loop_t* _qb_loop;
-        int _fd;
-        std::map<uint32_t, MessageObserver> _observers;
-    };
-
-    class Event
-    {
-    public:
-        Event(Notifier* notifier);
-        void process();
-    private:
-        std::string rcvMessage();
-
-        Notifier* _notifier;
-    };
-
-    #define QB_MS_TIMEOUT (500)
-    #define QB_BUF_SIZE (1 << 20)
 } /* namespace usbguardNotifier */

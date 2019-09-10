5211f3794825450a8ca9edc36bec6bce0f251652
Zoltan Fridrich <zfridric@redhat.com>
2019-09-10 10:11:57 +0200

    CLI - basic functionality

--- src/MainCli.cpp
+++ src/MainCli.cpp
@@ -44,16 +44,34 @@ int main(int argc, char** argv)
             return EXIT_FAILURE;
         }
     }
+
+    // Read config file
     usbguard::ConfigFile config(config_names);
     config.open(CONF_FILE, /*readonly=*/true);
 
-    Serializer serializer(config.getSettingValue("NotificationPath"));
-    CLI notifier(serializer.deserializeAll());
+    // Load notifications from file
+    std::string notification_path = config.getSettingValue("NotificationPath");
+    Serializer serializer(notification_path);
+    std::map<unsigned, Notification> map;
+    try {
+        map = serializer.deserializeAll();
+    } catch (std::runtime_error& e) {
+        std::cerr << "Error: file " << notification_path
+            << " doesnt exist." << std::endl;
+        return EXIT_FAILURE;
+    }
+    if (map.empty()) {
+        std::cout << "No notifications to show." << std::endl;
+        return EXIT_SUCCESS;
+    }
+    CLI notifier(map);
 
     CLI::Command cmd_code = CLI::Command::UNKNOWN;
     std::string line, cmd_name, cmd_options;
 
     while (cmd_code != CLI::Command::QUIT) {
+        std::cout << "& ";
+
         std::cin >> cmd_name;
         // TODO figure out how to eliminate whitespace
         std::getline(std::cin, cmd_options);
@@ -63,6 +81,9 @@ int main(int argc, char** argv)
             cmd_code = notifier.execute(cmd_name, cmd_options);
         } catch (std::runtime_error& e) {
             std::cerr << e.what() << std::endl;
+        }
+        if (cmd_code == CLI::Command::UNKNOWN) {
+            std::cerr << "Unknown command." << std::endl;
         }
     }
     return EXIT_SUCCESS;
